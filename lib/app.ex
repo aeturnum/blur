ee86595dde3af7708c0494aaee55d726fb24b6fa
Daniel Drexler <aeturnum@gmail.com>
2021-05-18 19:46:36 -0700

    Change conditions for checking the env for twitch key

--- lib/app.ex
+++ lib/app.ex
@@ -15,8 +15,12 @@ defmodule Blur.App do
   @impl true
   @spec start(atom, list) :: {:error, atom} | {:ok, pid()}
   def start(_type, opts) do
-    # validate environmental variables
-    if not Keyword.has_key?(opts, :twitch_client_key) do
+    # This function is called when the Application is started, which is not the same as a Bot starting.
+    # It is just going to start our Supervisor that will keep the bots running.
+    # So if we are expecting the twitch_client_key to be in the options we don't need to check
+    # the environment.
+    opts = Keyword.put(opts, :client_key_source, Application.get_env(:blur, :client_key_source, :env))
+    if Keyword.get(opts, :client_key_source) == :env do
       System.fetch_env!("TWITCH_CLIENT_KEY")
     end
 
